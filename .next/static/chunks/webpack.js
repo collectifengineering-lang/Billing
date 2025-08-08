95f45f9f3f944e56cccfa8e05885345b78d183b8
Collectif <collectifengineering@gmail.com>
2025-08-08 00:08:57 -0400

    Update app paths manifest to reflect new API routes and reorder existing routes. Modify webpack runtime hash for improved build consistency. Update layout component export for better project structure.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0dc9ac07c87a8e98"; }
+/******/ 		__webpack_require__.h = function() { return "d89d32f1831460fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
