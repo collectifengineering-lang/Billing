4f542662cac8d8ef354f9a58af7e19980a52fa0f
Collectif <collectifengineering@gmail.com>
2025-08-08 13:55:22 -0400

    Update webpack cache files and manifest to improve build consistency. Remove obsolete pack files and reorder routes in app paths manifest for better clarity. Modify webpack runtime hash for enhanced performance.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8f695ff3eaa2b1d"; }
+/******/ 		__webpack_require__.h = function() { return "bd09e10a00941e98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
